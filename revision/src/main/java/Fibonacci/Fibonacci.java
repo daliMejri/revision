2c7df84bc0a3e825ff4620e9c817654db13c9e85
dali <dali@mouhamed>
2014-05-27 16:26:39 +0200

    commit3
    Signed-off-by: dali <dali@mouhamed>

---
+++ revision/src/main/java/Fibonacci/Fibonacci.java
@@ -0,0 +1,12 @@
+package Fibonacci;
+
+public class Fibonacci {
+	public int process(int n) {
+		if (n < 2) {
+			return n;
+		} else
+			return process(n - 1) + process(n - 2);
+
+	}
+
+}
---
+++ revision/src/test/java/Fibonacci/Testfibo.java
@@ -0,0 +1,30 @@
+package Fibonacci;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class Testfibo {
+	private Fibonacci fibonacci;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		fibonacci = new Fibonacci();
+	}
+
+	@Test
+	public void itShouldReturn1when2entred() {
+		assertEquals(1, fibonacci.process(2));
+	}
+	
+	
+	@Test
+	public void itShouldReturn2when3entred() {
+		assertEquals(2, fibonacci.process(3));
+	}
+
+}
